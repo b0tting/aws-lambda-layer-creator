cff2d308fd762583af06b4119d5dfff0b4e85978
mark.otting <mark.otting@vopak.com>
2023-05-05 09:40:56 +0200

    FIX: Linting and shellcheck fixes

--- create-layer.sh
+++ create-layer.sh
@@ -21,16 +21,14 @@ fi
 
 if ! command -v docker &> /dev/null
 then
-  curl -fsSL https://get.docker.com -o get-docker.sh
+  curl -fsSL "https://get.docker.com" -o get-docker.sh
   sh ./get-docker.sh --dry-run
 fi
 
 echo "================================="
-
 echo "LayerName: $layername"
 echo "Runtime: $runtime"
 echo "Packages: $packages"
-
 echo "================================="
 
 host_temp_dir="$(mktemp -d)"
@@ -39,20 +37,16 @@ support_python_runtime=("python3.6,python3.7,python3.8,python3.9,python3.10,pyth
 
 support_node_runtime=("nodejs10.x,nodejs12.x,nodejs14.x,nodejs16.x,nodejs18.x")
 
-if [[ "${support_node_runtime[*]}" =~ "${runtime}" ]]; then
-
+if [[ "${support_node_runtime[*]}" == *"${runtime}"* ]]; then
     installation_path="nodejs"
     docker_image="public.ecr.aws/sam/build-$runtime:latest"
     echo "Preparing lambda layer"
     docker run --rm -v "$host_temp_dir:/lambda-layer" -w "/lambda-layer" "$docker_image" /bin/bash -c "mkdir $installation_path && npm install --prefix $installation_path --save $packages && zip -r lambda-layer.zip *"
-
-elif [[ "${support_python_runtime[*]}" =~ "${runtime}" ]]; then
-
+elif [[ "${support_python_runtime[*]}" == *"${runtime}"* ]]; then
     installation_path="python"
     docker_image="public.ecr.aws/sam/build-$runtime:latest"
     echo "Preparing lambda layer"
     docker run --rm -v "$host_temp_dir:/lambda-layer" -w "/lambda-layer" "$docker_image" /bin/bash -c "mkdir $installation_path && pip install $packages -t $installation_path  && zip -r lambda-layer.zip * -x '*/__pycache__/*'"
-
 else
     echo "Invalid runtime"
     exit 1
