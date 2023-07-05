94e2f69623f7fc918d161763ef8f4c17b8aea3bb
mark_gh@malkav.dds.nl <mark.otting@vopak.com>
2023-07-05 14:48:39 +0200

    FIX: Issue with removing temp files

--- create-layer.sh
+++ create-layer.sh
@@ -53,7 +53,7 @@ else
 fi
 
 echo "Deleting temporary files"
-docker run --rm -v "$host_temp_dir" "$docker_image" rm -rf "$host_temp_dir"
+docker run --rm -v "$host_temp_dir" "$docker_image" rm -rf "$host_temp_dir/*"
 echo "Deleted"
 
 mv "$host_temp_dir"/lambda-layer.zip "${layername}".zip
